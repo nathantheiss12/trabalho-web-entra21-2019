79b11df83546878d15bd3ae138facce84f03524b
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-07-15 14:28:52 -0300

    Configurado todo o Controller e o Repository do Usuario

--- Trabalho C Sharp/Repository/UsuarioRepository.cs
+++ Trabalho C Sharp/Repository/UsuarioRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,84 @@ namespace Repository
 {
     public class UsuarioRepository
     {
+        public List<Usuario> ObterTodos()
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"SELECT * FROM usuarios";
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            List<Usuario> usuarios = new List<Usuario>();
+
+            foreach(DataRow linha in tabela.Rows)
+            {
+                Usuario usuario = new Usuario();
+                usuario.Id = Convert.ToInt32(linha["id"]);
+                usuario.Nome = linha["nome"].ToString();
+                usuario.Login = linha["login"].ToString();
+                usuario.Senha = linha["senha"].ToString();
+                usuarios.Add(usuario);
+            }
+            comando.Connection.Close();
+            return usuarios;
+
+        }
+
+        public int Inserir(Usuario usuario)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"INSERT INTO usuarios (nome, login, senha) OUTPUT INSERTED.ID VALUES(@NOME, @LOGIN, @SENHA)";
+            comando.Parameters.AddWithValue("@NOME", usuario.Nome);
+            comando.Parameters.AddWithValue("@LOGIN", usuario.Login);
+            comando.Parameters.AddWithValue("@SENHA", usuario.Senha);
+            int id = Convert.ToInt32(comando.ExecuteScalar());
+            comando.Connection.Close();
+            return id;
+        }
+
+        public Usuario ObterPeloId(int id)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"SELECT * FROM usuarios WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            comando.Connection.Close();
+            if (tabela.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            DataRow linha = tabela.Rows[0];
+            Usuario usuario = new Usuario();
+            usuario.Id = Convert.ToInt32(linha["id"]);
+            usuario.Nome = linha["nome"].ToString();
+            usuario.Login = linha["login"].ToString();
+            usuario.Senha = linha["senha"].ToString();
+            return usuario;
+        }
+
+        public bool Alterar(Usuario usuario)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"UPDATE usuarios SET nome = @NOME, login = @LOGIN, senha = @SENHA WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", usuario.Id);
+            comando.Parameters.AddWithValue("@NOME", usuario.Nome);
+            comando.Parameters.AddWithValue("@LOGIN", usuario.Login);
+            comando.Parameters.AddWithValue("@SENHA", usuario.Senha);
+            int quantidadeAfetada = comando.ExecuteNonQuery();
+            comando.Connection.Close();
+            return quantidadeAfetada == 1;
+        }
+
+        public bool Apagar(int id)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"DELETE FEROM usuario WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            int quantidadeAfetada = comando.ExecuteNonQuery();
+            comando.Connection.Close();
+            return quantidadeAfetada == 1;
+        }
     }
 }
--- Trabalho C Sharp/View/Controllers/UsuarioController.cs
+++ Trabalho C Sharp/View/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,59 @@ namespace View.Controllers
 {
     public class UsuarioController : Controller
     {
+        private UsuarioRepository repository;
+
+        public UsuarioController()
+        {
+            repository = new UsuarioRepository();
+        }
+
         // GET: Usuario
         public ActionResult Index()
         {
+            List<Usuario> usuarios = repository.ObterTodos();
+            ViewBag.Usuarios = usuarios;
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+            repository.Inserir(usuario);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Usuario usuario = repository.ObterPeloId(id);
+            ViewBag.Usuario = usuario;
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = id;
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+
+            repository.Alterar(usuario);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
         }
     }
 }
