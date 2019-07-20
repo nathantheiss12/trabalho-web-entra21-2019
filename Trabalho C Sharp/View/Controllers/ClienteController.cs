7362e39bfde0fc2ec4b887657538d83d6f059108
HenriqueLinharesMontibeller <iquemontibeller@gmail.com>
2019-07-20 18:13:07 -0300

    Comecei a programar o controller dos clientes, Index, Cadastro e Store ja estão prontos

---
+++ Trabalho C Sharp/View/Controllers/ClienteController.cs
@@ -0,0 +1,51 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private ClienteRepository repository;
+
+        public ClienteController()
+        {
+            repository = new ClienteRepository();
+        }
+        public ActionResult Index()
+        {
+            List<Cliente> clientes = repository.ObterTodos();
+            ViewBag.Clientes = clientes;
+
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            CidadeRepository cidadeRepository = new CidadeRepository();
+            List<Cidade> cidades = cidadeRepository.ObterTodos();
+            ViewBag.Cidades = cidades;
+
+            return View();
+        }
+
+        public ActionResult Store(int cidade, string nome, string cpf, DateTime dataNascimento, int numero, string complemento, string logradouro, string cep)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Id_Cidade = cidade;
+            cliente.Nome = nome;
+            cliente.Cpf = cpf;
+            cliente.Data_Nascimento = dataNascimento;
+            cliente.Numero = numero;
+            cliente.Complemento = complemento;
+            cliente.Logradouro = logradouro;
+            cliente.Cep = cep;
+            repository.Inserir(cliente);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Trabalho C Sharp/View/Controllers/ProjetoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class ProjetoController : Controller
+    {
+        // GET: Projeto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
